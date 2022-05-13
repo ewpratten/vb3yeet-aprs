a908a43fcab8043b4c429f9b483563366a66e86c
Evan Pratten <ewpratten@gmail.com>
2022-05-13 10:19:51 -0400

    caddy n stuff

--- containers/router/router_setup.sh
+++ containers/router/router_setup.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 set -ex
 
+ip a
+ping -c 3 10.31.62.1
+
 # APRS inbound packet routing
 echo "Starting APRS inbound packet routing"
-socat UDP4-RECVFROM:10152,fork UDP4-SENDTO:$(APRS_UDP_SENDTO_ADDR):10152
+socat UDP4-RECVFROM:10152,fork UDP4-SENDTO:${APRS_UDP_SENDTO_ADDR}:10152 &
 
 # Web proxy connection routing
 echo "Starting Web proxy connection routing"
-socat UDP4-RECVFROM:80,fork UDP4-SENDTO:$(WEB_PROXY_UDP_SENDTO_ADDR):80
-socat UDP4-RECVFROM:443,fork UDP4-SENDTO:$(WEB_PROXY_UDP_SENDTO_ADDR):443
+socat TCP4-LISTEN:80,fork TCP4:${WEB_PROXY_TCP_SENDTO_ADDR}:80 &
+socat TCP4-LISTEN:443,fork TCP4:${WEB_PROXY_TCP_SENDTO_ADDR}:443
+
